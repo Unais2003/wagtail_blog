f0c65efe0ab05d7ad89d85850f9d5c1b77a55123
p01v21 <muhammed.perinchikkal@edu.fh-joanneum.at>
2025-12-01 00:45:19 +0100

    updated blog add and  edit, then  publish feature

--- Wagtail/trial/blog/models.py
+++ Wagtail/trial/blog/models.py
@@ -5,11 +5,21 @@ from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.models import Image
+from wagtail.images.edit_handlers import ImageChooserPanel
+
 
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = RichTextField(blank=True)
+    featured_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     body = StreamField([
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
@@ -18,6 +28,7 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
+        FieldPanel('featured_image'),
         FieldPanel('body'),
     ]
 
