65b1322fdda393ad5d2e3d1288e4c1ff88510f51
p01v21 <muhammed.perinchikkal@edu.fh-joanneum.at>
2025-11-30 16:33:19 +0100

    "initial commit after createating the project"

--- Wagtail/trial/blog/models.py
+++ Wagtail/trial/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.models import Page
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = RichTextField(blank=True)
+    body = StreamField([
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
